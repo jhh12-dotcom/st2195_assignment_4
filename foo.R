8a3b510c941abbf9b4299041e123f896b69c4356
JHH <jhh12@student.london.ac.uk>
2026-03-24 22:23:23 +0900

    Fixed volume formula: changed r^2 to r^3

--- foo.R
+++ foo.R
@@ -1,18 +1,16 @@
-# Radius
-r <- 2
 # Function to compute the volume of a sphere with radius r
-volume <- function(r, rho) {
-3/4*pi*r^2
+volume <- function(r) {
+  val <- (4/3) * pi * (r^3)
+  return(val)
 }
 
-# Function to compute the volumes of the spheres with radius r, r^2 and
-r^3
+# Function to compute the volumes of the spheres with radius r, r^2 andr^3
 volume_vector <- function(r) {
-r <- 22
-for (r in 2:4){
-volume(r)
+  radii <- c(r, r^2, r^3)
+  for (radius in radii) {
+    print(volume(radius))
+  }
 }
-}
-# Run volume_vector(r) and print the volumes of the spheres with radius
-r, r^2 and r^3
+# Run volume_vector(r) and print the volumes of the spheres with radius r, r^2 and r^3 
+r <- 2
 volume_vector(r)
